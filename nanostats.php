dcc94927e0aa0969feec06215edb5f8f7108eea2
camilstaps <camilstaps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-12 17:42:50 +0000

    1.2.8
    git-svn-id: https://plugins.svn.wordpress.org/nano-stats/tags/1.2.8@696680 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ nanostats.php
@@ -0,0 +1,429 @@
+<?php
+/*
+Plugin Name: NaNo Stats
+Plugin URI: http://wordpress.camilstaps.nl/plugins/nano-stats/
+Description: Allows you to show your NaNoWriMo Stats in posts, pages and sidebar widgets.
+Version: 1.2.8
+Author: Camil Staps
+Author URI: http://camilstaps.nl
+License: GPL2
+
+Copyright 2012  Camil Staps  (email : info@camilstaps.nl)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+error_reporting(E_ALL);
+
+/* Runs when plugin is activated */
+register_activation_hook(__FILE__,'nanostats_install');
+
+/* Runs on plugin deactivation*/
+register_deactivation_hook(__FILE__, 'nanostats_remove' );
+
+function nanostats_install() {
+	add_option('nanostats_username','camilstaps','','yes');
+	add_option('nanostats_region','europe-holland-belgium','','yes');
+	add_option('nanostats_width','600','','yes');
+	add_option('nanostats_height','350','','yes');
+	add_option('nanostats_color_wordcount','#8888cc','','yes');
+	add_option('nanostats_color_goal','#674732','','yes');
+	add_option('nanostats_color_daily','#000000','','yes');
+	add_option('nanostats_title','NaNoWriMo Stats for %u','','yes');
+}
+
+function nanostats_remove() {
+	delete_option('nanostats_username');
+	delete_option('nanostats_region');
+	delete_option('nanostats_width');
+	delete_option('nanostats_height');
+	delete_option('nanostats_color_wordcount');
+	delete_option('nanostats_color_goal');
+	delete_option('nanostats_color_daily');
+	delete_option('nanostats_title');
+}
+
+/* ADMIN MENU */
+
+if (is_admin()) {
+	/* Call the html code */
+	add_action('admin_menu', 'nanostats_admin_menu');
+
+	function nanostats_admin_menu() {
+		add_options_page('NaNo Stats Options', 'NaNo Stats', 'administrator','nanostats', 'nanostats_option_page');
+	}
+	
+	function nanostats_option_page() {
+		include(dirname(__FILE__).'/admin.php');
+	}
+}
+
+/* SHORTCODE */
+
+function objectsIntoArray($arrObjData, $arrSkipIndices = array()) {
+	$arrData = array();
+   
+	// if input is object, convert into array
+	if (is_object($arrObjData)) {
+		$arrObjData = get_object_vars($arrObjData);
+	}
+   
+	if (is_array($arrObjData)) {
+		foreach ($arrObjData as $index => $value) {
+			if (is_object($value) || is_array($value)) {
+				$value = objectsIntoArray($value, $arrSkipIndices);
+			}
+			if (in_array($index, $arrSkipIndices)) {
+				continue;
+			}
+			$arrData[$index] = $value;
+		}
+	}
+	return $arrData;
+}
+
+$nStats = 1;
+
+function showNaNoStats($atts) {
+	global $nStats;
+	if (isset($atts['width'])) {
+		$width = $atts['width'];
+	} else {
+		$width = get_option('nanostats_width');
+	}
+	if (isset($atts['height'])) {
+		$height = $atts['height'];
+	} else {
+		$height = get_option('nanostats_height');
+	}
+	if (isset($atts['username'])) {
+		$username = $atts['username'];
+	} else {
+		$username = get_option('nanostats_username');
+	}
+	if (isset($atts['wccolor'])) {
+		$wccolor = $atts['wccolor'];
+	} else {
+		$wccolor = get_option('nanostats_color_wordcount');
+	}
+	if (isset($atts['goalcolor'])) {
+		$goalcolor = $atts['goalcolor'];
+	} else {
+		$goalcolor = get_option('nanostats_color_goal');
+	}
+	if (isset($atts['dailycolor'])) {
+		$dailycolor = $atts['dailycolor'];
+	} else {
+		$dailycolor = get_option('nanostats_color_daily');
+	}
+	if (isset($atts['showtitle'])) {
+		$showtitle = $atts['showtitle'];
+	} else {
+		$showtitle = 'true';
+	}
+	if (isset($atts['showtotals'])) {
+		$showtotals = $atts['showtotals'];
+	} else {
+		$showtotals = 'true';
+	}
+	if (isset($atts['showdaily'])) {
+		$showdaily = $atts['showdaily'];
+	} else {
+		$showdaily = 'true';
+	}
+	if (isset($atts['showgoals'])) {
+		$showgoals = $atts['showgoals'];
+	} else {
+		$showgoals = 'true';
+	}
+	
+	if ($showtitle=='true') {
+		$title = get_option('nanostats_title');
+		$title = str_replace('%u',$username,$title);
+	}
+	
+	$xmlUrl = "http://nanowrimo.org/wordcount_api/wchistory/".$username; 
+    $xmlStr = '';
+    $c = 0;
+    while ($xmlStr=='') {
+        if ($c==3) $xmlStr == 'NODATA';
+        $xmlStr = file_get_contents($xmlUrl);
+        $c++;
+    }
+    if ($xmlStr=='NODATA') {
+        $return = "Couldn't get the statistics for ".$username." from NaNoWriMo.";
+    } else {
+        $xmlObj = simplexml_load_string($xmlStr);
+        $nanodata = objectsIntoArray($xmlObj);
+        $data = $nanodata['wordcounts'];
+        
+        foreach ($data as $dat) {
+            $data2[] = $dat;
+        }
+        $data = $data2;
+        $data = $data[0];
+        
+        $wordcounts = '';
+        $dailywc = '';
+        $goals = '';
+        $wordcount = 0;
+        $year = substr($data[0]['wcdate'],0,4);
+        $current = false;
+        if (date('Y-m')==$year.'-11') $current = true;
+        $entry = 0;
+        for ($day=1;$day!=31;$day++) {
+            # dailywc
+            if ($day<10) {
+                $dayFormat = '0'.$day;
+            } else {
+                $dayFormat = $day;
+            }
+            $dailywc .= '['.$day.',';
+            if (isset($data[$entry]['wcdate']) && $data[$entry]['wcdate'] == $year.'-11-'.$dayFormat) {
+                $wordcount += $data[$entry]['wc'];
+                $dailywc .= $data[$entry]['wc'];
+                $entry++;
+            } else {
+                $dailywc .= 0;
+            }
+            $dailywc .= ']';
+            if ($day!=30) $dailywc .= ',';
+            
+            # wordcount
+            $wordcounts .= '['.$day.',';
+            if (!$current || date('d')>=$day) {
+                $wordcounts .= $wordcount;
+            } else {
+                $wordcounts .= 0;
+            }
+            $wordcounts .= ']';
+            if ($day!=30) $wordcounts .= ',';
+            
+            # goal
+            $goal = round(50000/30*$day);
+            $goals .= '['.$day.','.$goal.']';
+            if ($day!=30) $goals .= ',';
+        }
+        
+        $max = array();
+        if ($showtotals!='false') $max[] = 10000*ceil($wordcount*1.1/10000);
+        if ($showdaily!='false') $max[] = 10000*ceil($data[$entry-1]['wc']*1.1/10000);
+        if ($showgoals!='false') $max[] = 10000*ceil($goal*1.1/10000);
+        $max = max($max);
+        if ($max>10000) $numberTicks = $max/10000 + 1;
+        if ($max<=10000) $numberTicks = $max/1000 + 1;
+        
+        $barWidth = floor(($width-50)/30-8);
+        
+        $showlines = '';
+        if ($showtotals!='false') $showlines .= ',totals';
+        if ($showdaily!='false') $showlines .= ',daily';
+        if ($showgoals!='false') $showlines .= ',goals';
+        $showlines = '['.substr($showlines,1).']';
+        
+        $showcolors = '';
+        if ($showtotals!='false') $showcolors .= ',"'.$wccolor.'"';
+        if ($showdaily!='false') $showcolors .= ',"'.$dailycolor.'"';
+        if ($showgoals!='false') $showcolors .= ',"'.$goalcolor.'"';
+        $showcolors = '['.substr($showcolors,1).']';
+        
+        $showseriesoptions = '';
+        if ($showtotals!='false') $showseriesoptions .= ',{
+                                renderer: jQuery.jqplot.BarRenderer,
+                                rendererOptions: {
+                                    barWidth: '.$barWidth.',
+                                    barPadding: -'.$barWidth.'
+                                },
+                                highlighter: { formatString: "Day: %s, total: %d" }
+                            }';
+        if ($showdaily!='false') $showseriesoptions .= ',{
+                                renderer: jQuery.jqplot.BarRenderer,
+                                rendererOptions: {
+                                    barWidth: '.$barWidth.',
+                                    barPadding: -'.$barWidth.'
+                                },
+                                highlighter: { formatString: "Day: %s, words written: %d" }
+                            }';
+        if ($showgoals!='false') $showseriesoptions .= ',{
+                                markerOptions: {
+                                    size: 0
+                                },
+                                highlighter: { formatString: "Day: %s, goal: %d", showMarker: true }
+                            }';
+        $showseriesoptions = '['.substr($showseriesoptions,1).']';
+        
+        wp_enqueue_script('jquery');
+        wp_enqueue_script('jqplot.min',plugins_url().'/nano-stats/jquery.jqplot.min.js');
+        wp_enqueue_script('jqplot.barRenderer.min',plugins_url().'/nano-stats/jqplot.barRenderer.min.js');
+        wp_enqueue_script('jqplot.categoryAxisRenderer.min',plugins_url().'/nano-stats/jqplot.categoryAxisRenderer.min.js');
+        wp_enqueue_script('jqplot.canvasAxisTickRenderer.min',plugins_url().'/nano-stats/jqplot.canvasAxisTickRenderer.min.js');
+        wp_enqueue_script('jqplot.canvasTextRenderer.min',plugins_url().'/nano-stats/jqplot.canvasTextRenderer.min.js');
+        wp_enqueue_script('jqplot.highlighter.min',plugins_url().'/nano-stats/jqplot.highlighter.min.js');
+        
+        $return = '
+        <link class="include" rel="stylesheet" type="text/css" href="'.plugin_dir_url(__FILE__).'jquery.jqplot.min.css" />
+
+        <div id="nanostats-'.$nStats.'" style="width:'.$width.'px; height:'.$height.'px;"></div>
+        <script class="code" type="text/javascript">
+            jQuery(document).ready(function(){
+                var daily = ['.$dailywc.'];
+                var totals = ['.$wordcounts.'];
+                var goals = ['.$goals.'];
+                
+                var plot1 = jQuery.jqplot(\'nanostats-'.$nStats.'\', '.$showlines.', {
+                    highlighter: {
+                        show: true,
+                        showMarker: false
+                    },
+                    seriesColors: '.$showcolors.',
+                    series:'.$showseriesoptions.',
+                    title: {
+                        text: "'.$title.'",
+                        show: '.$showtitle.'
+                    },
+                    axes: {
+                        xaxis: {
+                            renderer: jQuery.jqplot.CategoryAxisRenderer
+                        },
+                        yaxis: {
+                            min: 0,
+                            max: '.$max.',
+                            numberTicks: '.$numberTicks.',
+                            tickOptions: {formatString: \'%d\'}
+                        }
+                    }
+                });
+            });
+        </script>';
+        $nStats++;
+    }
+	return $return;
+}
+
+function showNaNoWidget_participant($atts) {
+	if (isset($atts['username'])) {
+		$username = $atts['username'];
+	} else {
+		$username = get_option('nanostats_username');
+	}
+	if ($atts['showpercent']=='true') $extra .= ',pc';
+	if ($atts['showdays']=='true') $extra .= ',days';
+	if ($atts['showwords']!='true') $extra .= ',wc';
+	if (isset($atts['goal'])) $extra .= ',goal='.$atts['goal'];
+	if ($atts['show_username']=='true') {
+		return '<img class="nanowidget" style="border:0;" src="http://www.nanowrimo.org/widget/LiveSupporter/'.$username.$extra.'.png" alt="NaNoWriMo Participant Widget"/>';
+	} else {
+		return '<img class="nanowidget" style="border:0;" src="http://www.nanowrimo.org/widget/LiveParticipant/'.$username.$extra.'.png" alt="NaNoWriMo Participant Widget"/>';
+	}
+}
+
+function showNaNoWidget_calendar($atts) {
+	if (isset($atts['username'])) {
+		$username = $atts['username'];
+	} else {
+		$username = get_option('nanostats_username');
+	}
+	if ($atts['showpercent']=='true') $extra .= ',pc';
+	if ($atts['showdays']=='true') $extra .= ',days';
+	if ($atts['showwords']!='true') $extra .= ',wc';
+	if (isset($atts['goal'])) $extra .= ',goal='.$atts['goal'];
+	return '<img class="nanowidget" style="border:0;" src="http://www.nanowrimo.org/widget/MyMonth/'.$username.$extra.'.png" alt="NaNoWriMo Calendar Widget"/>';
+}
+
+function showNaNoWidget_progress($atts) {
+	if (isset($atts['username'])) {
+		$username = $atts['username'];
+	} else {
+		$username = get_option('nanostats_username');
+	}
+	if ($atts['showpercent']=='true') $extra .= ',pc';
+	if ($atts['showdays']=='true') $extra .= ',days';
+	if ($atts['showwords']!='true') $extra .= ',wc';
+	if (isset($atts['goal'])) $extra .= ',goal='.$atts['goal'];
+	return '<img class="nanowidget" style="border:0;" src="http://www.nanowrimo.org/widget/graph/'.$username.$extra.'.png" alt="NaNoWriMo Progress Widget"/>';
+}
+
+function showNaNoWidget_wordwar($atts) {
+	$i = 1;
+	while (isset($atts['username'.$i])) {
+		$usernames .= ','.$atts['username'.$i];
+		$i++;
+	}
+	if ($i<3) $usernames .= ','.get_option('nanostats_username');
+	$usernames = substr($usernames,1);
+	
+	if ($atts['showpercent']=='true') $extra .= ',pc';
+	if ($atts['showdays']=='true') $extra .= ',days';
+	if ($atts['showwords']!='true') $extra .= ',wc';
+	if (isset($atts['goal'])) $extra .= ',goal='.$atts['goal'];
+	
+	return '<img class="nanowidget" style="border:0;" src="http://www.nanowrimo.org/widget/WordWar/'.$usernames.$extra.'.png" alt="NaNoWriMo WordWar Widget"/>';
+}
+
+function showNaNoWidget_regionstatus($atts) {
+	if (isset($atts['region'])) {
+		$region = $atts['region'];
+	} else {
+		$region = get_option('nanostats_region');
+	}
+	
+	if ($atts['showpercent']=='true') $extra .= ',pc';
+	if ($atts['showdays']=='true') $extra .= ',days';
+	if ($atts['showwords']!='true') $extra .= ',wc';
+	if (isset($atts['goal'])) $extra .= ',goal='.$atts['goal'];
+	
+	return '<img class="nanowidget" style="border:0;" src="http://www.nanowrimo.org/widget/RegionStatus/'.$region.$extra.'.png" alt="NaNoWriMo RegionStatus Widget"/>';
+}
+
+function showNaNoWidget_regionwar($atts) {
+	$i = 1;
+	while (isset($atts['region'.$i])) {
+		$regions .= ','.$atts['region'.$i];
+		$i++;
+	}
+	if ($i<3) $regions .= ','.get_option('nanostats_region');
+	$regions = substr($regions,1);
+	
+	if ($atts['showpercent']=='true') $extra .= ',pc';
+	if ($atts['showdays']=='true') $extra .= ',days';
+	if ($atts['showwords']!='true') $extra .= ',wc';
+	if (isset($atts['goal'])) $extra .= ',goal='.$atts['goal'];
+	
+	return '<img class="nanowidget" style="border:0;" src="http://www.nanowrimo.org/widget/RegionWar/'.$regions.$extra.'.png" alt="NaNoWriMo RegionWar Widget"/>';
+}
+
+function showNaNoStats_wordcount($atts) {
+	if (isset($atts['username'])) {
+		$username = $atts['username'];
+	} else {
+		$username = get_option('nanostats_username');
+	}
+	
+	$xmlUrl = "http://nanowrimo.org/wordcount_api/wc/".$username; 
+	$xmlStr = file_get_contents($xmlUrl);
+	$xmlObj = simplexml_load_string($xmlStr);
+	$nanodata = objectsIntoArray($xmlObj);
+	return $nanodata['user_wordcount'];
+}
+
+add_shortcode('nanostats','showNaNoStats');
+add_shortcode('nanowidget_participant','showNaNoWidget_participant');
+add_shortcode('nanowidget_calendar','showNaNoWidget_calendar');
+add_shortcode('nanowidget_progress','showNaNoWidget_progress');
+add_shortcode('nanowidget_wordwar','showNaNoWidget_wordwar');
+add_shortcode('nanowidget_regionstatus','showNaNoWidget_regionstatus');
+add_shortcode('nanowidget_regionwar','showNaNoWidget_regionwar');
+add_shortcode('nanostats_wordcount','showNaNoStats_wordcount');
+?>
---
+++ readme.txt
@@ -0,0 +1,93 @@
+=== NaNo Stats ===
+Contributors: camilstaps
+Tags: nanowrimo, statistics, nano, stats, wrimo
+Donate link: http://plugins.camilstaps.nl/donate
+Requires at least: 2.5
+Tested up to: 3.4.1
+Stable tag: 1.2.8
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Allows you to show your NaNoWriMo Stats in posts, pages and sidebar widgets.
+
+== Description ==
+
+Allows you to show your NaNoWriMo Stats in posts, pages and sidebar widgets. Also, NaNoWriMo widgets are available.
+
+The NaNo Stats plugin comes with clear explanation and documentation.
+
+This is an unofficial plugin, not made by the Office of Letters and Light.
+
+== Installation ==
+
+1. Upload folder `nano-stats` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= What is NaNoWriMo? =
+NaNoWriMo is short for National Novel Writing Month. It is a worldwide contest for (hobby)writers to write 50,000 words in one month, November. For more information, please visit http://nanowrimo.org.
+
+= My charts and widgets are loading slowly. What can be done about this? =
+I'm afraid this is not an issue of the plugin but of http://nanowrimo.org. During November, this site gets a huge load of data traffic, which causes it to shutdown quiet often during this month. There is nothing that can be done about this, except improving the NaNoWriMo servers.
+
+= Can I use the plugin for Camp NaNoWriMo as well? =
+Unfortunately, [Camp NaNoWriMo](http://www.campnanowrimo.org) does _not_ provide statistics info. So no, you cannot use this plugin with Camp NaNoWriMo - hopefully in the future!
+
+== Screenshots ==
+
+1. With an easy shortcode, you can add this awesome stats chart to your site! Colors, size and title are customizable.
+2. The settings page is clear and simple.
+
+== Changelog ==
+
+= 1.2.8 =
+* Added FAQ about Camp NaNoWriMo
+
+= 1.2.7 =
+* Updated all author links
+
+= 1.2.6 =
+* Minor improvement. 
+* Updated the FAQ.
+
+= 1.2.5 =
+* Bug fixes. Sorry for the inconvenience. 
+
+= 1.2.4 =
+* Another bug fix - sorry for the many updates; as this is a plugin dedicated for November, it's quite hard testing. Thanks for your understanding!
+
+= 1.2.3 =
+* Bug fix.
+
+= 1.2.2 =
+* Minor improvement on the settings page.
+
+= 1.2.1 =
+* Changed the name to NaNo Stats on request of the Office of Letters and Light.
+
+= 1.2 =
+* A nice new settings page, with tabs.
+
+= 1.1 =
+* Added daily wordcount bars
+* Added option to toggle visibility of totals, goals and daily wordcounts
+* Better y axis labels
+
+= 1.0.4 =
+* Fixed bugs in the wordwar widgets
+* Updated donate link
+
+= 1.0.3 =
+* Fixed bug that made options unsavable
+
+= 1.0.2 =
+* Added donate link
+* Added plugin link
+* Changed the old name "NaNo Stats" into "NaNoWriMo Stats" everywhere
+
+= 1.0.1 =
+Updated readme file
+
+= 1.0 =
+Yay, first version!
